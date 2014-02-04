136af757e5535aa617090da80ec660ff9895c0d4
snoller <stephan.noller@gmail.com>
2014-02-04 18:20:16 +0100

    button auf interrupt-lösung geändert

--- targetometer_button.py
+++ targetometer_button.py
@@ -10,29 +10,12 @@ GPIO.setwarnings(False)
 
 GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-switch = 0
-lastpushed = 0
-while True:
-	if GPIO.input(25):
-		if (time.time() - lastpushed < 0.3):	
-			switch = 2
-			print "double"
-		elif (time.time() - lastpushed > 2):
-			switch = 1
-			print "single"
-		lastpushed = time.time()
-	#print "lastpushed: " + str(lastpushed) + " time: " + str(time.time())	
-	#print "\ntimediff: " + str(time.time() - lastpushed)
-	if switch==1:
-		os.system("python /home/pi/targetometer/targetometer_button_once.py")
-		switch = 0
-	if switch==2:
-		lcd.clear()
-		lcd.message("ok we'll contact\nyou asap")
-		sleep(1)
-		lcd.clear()
-		switch = 0
-	sleep(0.1)
+try:
+ while True:
+  GPIO.wait_for_edge(25, GPIO.FALLING)
+  os.system("python /home/pi/targetometer/targetometer_button_once.py")
+
+except KeyboardInterrupt:
+ GPIO.cleanup()
 
 GPIO.cleanup()
-
